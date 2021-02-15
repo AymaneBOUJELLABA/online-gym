a7d4a94587150fdcdd9ac2c0b274c7821a741441
ferdaousak <akkar88889@gmail.com>
2021-02-15 14:52:58 +0100

    add interface and conrollers

---
+++ src/main/java/com/gym/app/repositories/UserRepository.java
@@ -0,0 +1,16 @@
+package com.gym.app.repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.gym.app.entities.Gymclass;
+import com.gym.app.entities.User;
+
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> 
+{
+	Optional<User> findById(String id);
+}
